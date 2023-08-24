a486a1c6d760666e321c0046a0a27b5b575932f9
reece00 <37351410+reece00@users.noreply.github.com>
2023-08-24 13:16:56 +0800

    Update chat.tsx

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -940,8 +940,6 @@ function _Chat() {
     const isTouchTopEdge = e.scrollTop <= edgeThreshold;
     const isTouchBottomEdge = bottomHeight >= e.scrollHeight - edgeThreshold;
     const isHitBottom = bottomHeight >= e.scrollHeight - (isMobileScreen ? 0 : 10);
-console.log("bottomHeight:", bottomHeight, "scrollHeight:", e.scrollHeight, "isMobileScreen:", isMobileScreen);
-console.log("isHitBottom:", isHitBottom);
     const prevPageMsgIndex = msgRenderIndex - CHAT_PAGE_SIZE;
     const nextPageMsgIndex = msgRenderIndex + CHAT_PAGE_SIZE;
 
