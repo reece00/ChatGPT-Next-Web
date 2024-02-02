d786fb3cd1d8399a2ed452bb9b7519b191dca852
reece00 <37351410+reece00@users.noreply.github.com>
2024-02-02 22:32:56 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -97,6 +97,12 @@ export const DEFAULT_MODELS = [
   {
     name: "gpt-3.5-turbo-16k-0613",
     available: true,
+  },{
+    name: "gpt-3.5-turbo-0125",
+    available: true,
+  },{
+    name: "gpt-4-0125-preview",
+    available: true,
   },
 ] as const;
 
