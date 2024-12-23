13de55b2c46b0efaf834ec02da912e229b6a9b56
reece00 <37351410+reece00@users.noreply.github.com>
2024-12-23 22:28:32 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -67,6 +67,10 @@ export const DEFAULT_MODELS = [
     available: false,
   },
   {
+    name: "gpt-3.5-turbo",
+    available: false,
+  },
+  {
     name: "gpt-4-1106-preview",
     available: true,
   },
